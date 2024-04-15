94cdad54f70fd623e2b3867bc26fe8c752d0e9b5
deltaGbi <146318165+deltaGbi@users.noreply.github.com>
2024-04-15 18:49:53 +0100

    Update Badge.tsx
    branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://aiworker.eu'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Aiworker</span>
       </a>
     </span>
   );
